b46293673eb89f85e590567399bc6c6807bc0cfd
juan-suarez <juan.suarez1@utp.edu.co>
2024-08-09 17:44:31 -0500

    download cv fixed

--- src/components/mainHeader/downloadCV.tsx
+++ src/components/mainHeader/downloadCV.tsx
@@ -3,7 +3,7 @@ import { Button } from '@mui/material';
 export const DownloadCV = () => {
   const handleDownload = () => {
     // URL del archivo PDF que quieres descargar
-    const pdfUrl = 'https://github/juan-suarez/Portfolio/main/public/CV.pdf';
+    const pdfUrl = '/CV.pdf';
     
     // Crear un enlace y hacer clic en él para iniciar la descarga
     const link = document.createElement('a');
